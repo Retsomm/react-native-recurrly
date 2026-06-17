85f989796269ef48e0bc0a332b6f46271e341fdd
Retsomm <112182ssss@gmail.com>
2026-06-17 20:55:15 +0800

    implement fonts

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -5,13 +5,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
     return (
-        <SafeAreaView className="flex-1 items-center justify-center bg-background">
-            <Text className="text-xl font-bold text-success">
+        <SafeAreaView className="flex-1 bg-background　p-5">
+            <Text className="text-5xl font-sans-extrabold">
                 Happy Coding!!!
             </Text>
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
-            <Link href="/subscriptions/claude" className="mt-4 rounded bg-primary text-white p-4">Claude Max Subscription</Link>
+            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4 font-sans-bold">Go to Sign Up</Link>
+            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4 font-sans-bold">Go to Sign In</Link>
+            <Link href="/subscriptions/claude" className="mt-4 rounded bg-primary text-white p-4 font-sans-bold">Claude Max Subscription</Link>
         </SafeAreaView>
     );
 }
